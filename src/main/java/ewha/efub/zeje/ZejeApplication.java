d693b7df983d7c5f7e0156bab253ac87e487aabb
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-06-19 13:33:51 +0900

    Initial commit

---
+++ src/main/java/ewha/efub/zeje/ZejeApplication.java
@@ -0,0 +1,13 @@
+package ewha.efub.zeje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZejeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZejeApplication.class, args);
+	}
+
+}
