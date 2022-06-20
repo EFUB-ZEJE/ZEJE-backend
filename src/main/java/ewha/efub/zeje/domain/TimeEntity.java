1e27ebf2be1b7e794d02c34d28db12a04f90578c
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-06-20 21:54:33 +0900

    Add : "기본 폴더 구조 생성 및 mysql 연결"
    기본적인 config, domain, service등의 package만듦

---
+++ src/main/java/ewha/efub/zeje/domain/TimeEntity.java
@@ -0,0 +1,16 @@
+package ewha.efub.zeje.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+public class TimeEntity {
+}
---
+++ src/main/java/ewha/efub/zeje/domain/User.java
@@ -0,0 +1,12 @@
+package ewha.efub.zeje.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+public class User{
+}
