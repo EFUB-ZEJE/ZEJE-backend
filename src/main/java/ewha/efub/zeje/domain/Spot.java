519286cb4978e8e2b27de6df56b49e3d9d66dec1
MINJU-KIMmm <0112km@ewhain.net>
2022-06-22 00:24:23 +0900

    Add : Spot domain & repository

---
+++ src/main/java/ewha/efub/zeje/domain/Spot.java
@@ -0,0 +1,49 @@
+package ewha.efub.zeje.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "spot")
+public class Spot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long spotId;
+
+    @Column(name = "content_id")
+    private Long contentId;
+
+    @NotNull
+    @Column(length = 45)
+    private String category;
+
+    @NotNull
+    @Column(length = 45)
+    private String name;
+
+    @NotNull
+    @Column
+    private String location;
+
+    @Column
+    private String description;
+
+    @Column
+    private String link;
+
+    @Builder
+    public Spot(Long contentId, String category, String name, String location, String description, String link) {
+        this.contentId = contentId;
+        this.category = category;
+        this.name = name;
+        this.location = location;
+        this.description = description;
+        this.link = link;
+    }
+}
