3a0b8b5ba22b386c830ec20d8ee4d3a20cffcd18
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-06-23 13:28:44 +0900

    Fix(TimeEntity): "Application에 @EnableJpaAuditing 붙임"
    zejeApplication에 @EnableJpaAuditing 붙여야 자동으로 생성한다는걸 까먹었음
    
    fixes #3

--- src/main/java/ewha/efub/zeje/domain/TimeEntity.java
+++ src/main/java/ewha/efub/zeje/domain/TimeEntity.java
@@ -1,13 +1,10 @@
 package ewha.efub.zeje.domain;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -18,7 +15,6 @@ import java.time.LocalDateTime;
 public class TimeEntity {
 
     @CreatedDate
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateSerializer.class)
+    @Column(nullable = false)
     private LocalDateTime createdDate;
 }
--- src/main/java/ewha/efub/zeje/domain/User.java
+++ src/main/java/ewha/efub/zeje/domain/User.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
