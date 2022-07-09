c20296cb3f7e1bdfefae00792dd79e206f247ba7
MINJU-KIMmm <0112km@ewhain.net>
2022-07-09 23:01:25 +0900

    Fix : 어노테이션 버그 수정
    1. 버그 나는 어노테이션 삭제
    2. Repository 어노테이션 추가
    3. Table 어노테이션 추가

--- src/main/java/ewha/efub/zeje/domain/DiaryRepository.java
+++ src/main/java/ewha/efub/zeje/domain/DiaryRepository.java
@@ -1,6 +1,8 @@
 package ewha.efub.zeje.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 }
