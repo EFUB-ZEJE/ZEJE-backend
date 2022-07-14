61dce499adf4e562542e26153d67cf604f5aad9a
MINJU-KIMmm <0112km@ewhain.net>
2022-07-14 17:23:48 +0900

    Feat : 에러 Custom 추가

---
+++ src/main/java/ewha/efub/zeje/util/errors/CustomException.java
@@ -0,0 +1,11 @@
+package ewha.efub.zeje.util.errors;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
+
