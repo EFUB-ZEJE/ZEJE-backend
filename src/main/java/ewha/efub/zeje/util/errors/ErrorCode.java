302b3879ba274c6f180ba150a68af4a624d91035
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-07-19 22:25:14 +0900

    New & Refactor: errorcode 추가 및 에러핸들링 완료

--- src/main/java/ewha/efub/zeje/util/errors/ErrorCode.java
+++ src/main/java/ewha/efub/zeje/util/errors/ErrorCode.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @Getter
 @AllArgsConstructor
@@ -17,12 +17,22 @@ public enum ErrorCode {
     INVALID_IMAGE_FILE(BAD_REQUEST, "잘못된 이미지 파일입니다."),
     INVALID_SESSION_USER(BAD_REQUEST, "세션 유저가 비어있습니다."),
 
+    //401 UNAUTHORIZED : 비인증 상태
+    USER_UNAUTHORIZED(UNAUTHORIZED, "로그인이 필요합니다."),
+
+    //403 FORBIDDEN : 권한 없음
+    ACCESS_DENIED(FORBIDDEN, "해당 페이지를 요청할 권한이 없습니다."),
+
     //404 NOT_FOUND : Resource를 찾을 수 없음
     USER_NOT_FOUND(NOT_FOUND, "해당 유저 정보를 찾을 수 없습니다."),
     REVIEW_NOT_FOUND(NOT_FOUND, "해당 리뷰 정보를 찾을 수 없습니다."),
     SPOT_NOT_FOUND(NOT_FOUND, "해당 스팟 정보를 찾을 수 없습니다."),
     DIARY_NOT_FOUND(NOT_FOUND, "해당 다이어리 정보를 찾을 수 없습니다."),
     MEMORY_NOT_FOUND(NOT_FOUND, "해당 일기 정보를 찾을 수 없습니다."),
+
+    //500 INTERNAL_SERVER_ERROR : 서버 내 문제
+    EXCEPTION(INTERNAL_SERVER_ERROR, "서버 내에 알 수 없는 오류가 발생했습니다."),
+    IO_EXCEPTION(INTERNAL_SERVER_ERROR, "이미지 업로드/다운로드 중 알 수 없는 오류가 발생했습니다.")
     ;
 
     private final HttpStatus httpStatus;
