4478c06b372b97cb9f6020fe839f261ad3245330
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2023-03-14 18:17:58 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ CURRENT_PID=$(sudo docker container ls -q)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." 
 else
   echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
   sudo docker stop $CURRENT_PID
